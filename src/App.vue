5b2d5c2a093b4e34b3dc7824ddf340a72f87af9d
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2022-07-19 19:37:32 +0200

    base style + some layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/football">Football</router-link>
+      <router-link class="nav-button"  to="/">Home</router-link>
+      <router-link class="nav-button" to="/about-me">Profile</router-link>
+      <router-link class="nav-button"  to="/football">Football</router-link>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 export default {}
 </script>
+
+<style lang="scss">
+  @import url(./assets/scss/index.module.scss);
+</style>
